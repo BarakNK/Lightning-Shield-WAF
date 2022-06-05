13509f37c43bf4f4682a54eaa9ac68144525a8c5
Barak_NK <barakuch28032004@gmail.com>
2022-06-06 02:03:35 +0300

    Added parsers for headers and cookies

--- request_parser.py
+++ request_parser.py
@@ -1,4 +1,26 @@
 
+def parse_headers(headers_list):
+    headers = {}
+    for line in headers_list:
+        if line != "":
+            key, value = line.split(": ")
+            headers[key] = value
+    return headers
+
+
+def parse_cookie(cookie_header):
+    if cookie_header is None:
+        return None
+    cookies_dict = {}
+    cookies_list = cookie_header.split(";")
+    for cookie in cookies_list:
+        splitted_cookie = cookie.split("=")
+        cookie_key = splitted_cookie[0]
+        cookie_value = splitted_cookie[1]
+        cookies_dict[cookie_key] = cookie_value
+    return cookies_dict
+
+
 def parse_request(http_request_str):
     http_request_list = http_request_str.splitlines()
     http_type, path = http_request_list[0].split(" ")[0:2]

eeaab04bc0ae7e76739e327986f6dcd18c03f3b4
Barak_NK <barakuch28032004@gmail.com>
2022-06-06 02:05:11 +0300

    Special parser for POST - HTTP requests

--- request_parser.py
+++ request_parser.py
@@ -1,3 +1,4 @@
+from requests_toolbelt.multipart import decoder
 
 def parse_headers(headers_list):
     headers = {}
@@ -7,6 +8,31 @@ def parse_headers(headers_list):
             headers[key] = value
     return headers
 
+
+def parse_post(post_request):
+    post_request_parts = post_request.split("\r\n\r\n")
+    header_str = post_request_parts[0]
+    headers = parse_headers(header_str.splitlines()[1:]) #[1:] because first line is GET and path
+    data_str = '\r\n\r\n'.join(post_request_parts[1:])
+    post_data = {}
+
+    if "multipart/form-data" in headers["Content-Type"]: #if information passes in multipule lines
+        boundary = headers["Content-Type"].split("boundary=")[1]
+        content_type = headers["Content-Type"]
+        result = decoder.MultipartDecoder(data_str.encode(), content_type)
+
+        for parameter in result.parts:
+            key = parameter.headers[b"Content-Disposition"].decode().split('name=\"')[1][0:-1]
+            post_data[key] = parameter.text
+ 
+    else: #if information passes regularly
+        no_spaces_str = ''.join(data_str.split())
+        parameters = no_spaces_str.split("&")
+        for parameter in parameters:
+            key, value = parameter.split("=")
+            post_data[key] = value
+
+    return {"headers": headers, "data": post_data}
 
 def parse_cookie(cookie_header):
     if cookie_header is None:
@@ -19,7 +45,6 @@ def parse_cookie(cookie_header):
         cookie_value = splitted_cookie[1]
         cookies_dict[cookie_key] = cookie_value
     return cookies_dict
-
 
 def parse_request(http_request_str):
     http_request_list = http_request_str.splitlines()
